1d76c32ee6c812a7034528c96d676551bad5e111
pauluskash <160007822+pewpewpau@users.noreply.github.com>
2026-07-30 21:58:45 +0200

    pages: login, forgotPassword, resetPassword, etc.

---
+++ webapp/src/main.ts
@@ -0,0 +1,8 @@
+import './css/style.css';
+import './ts/forgotpassword';
+import './ts/login';
+import './ts/otp';
+import './ts/resetPassword';
+import './ts/validation';
+
+console.log('Admin Login UI Loaded');
